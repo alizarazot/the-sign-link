6214b974145e6a31b7c3d81bf228134ff8620c83
alizarazot <alizarazot@ufpso.edu.co>
2024-04-21 15:17:35 -0500

    Include the Service Worker in TSConfig

--- src/internal/service-worker/service-worker.ts
+++ src/internal/service-worker/service-worker.ts
@@ -1,8 +1,6 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
 /// <reference lib="webworker" />
 
-import * as logging from "../../pkg/logging";
+import * as logging from "pkg/logging";
 
 const log = logging.setDefaultLogger(
   new logging.Logger("Service-Worker", logging.Level.Debug),

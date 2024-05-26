3ca259ca73b01b904686efc0136275a6cc677969
alizarazot <alizarazot@ufpso.edu.co>
2024-05-26 13:08:25 -0500

    Fix stats view

--- src/view/stats.ts
+++ src/view/stats.ts
@@ -1,9 +1,10 @@
 import { LitElement, css, html } from "lit";
-import { customElement, property, query } from "lit/decorators.js";
+import { customElement, query, state } from "lit/decorators.js";
 
 import { IgcListComponent, defineComponents } from "igniteui-webcomponents";
 
 import { Lesson } from "lesson";
+import { currentSession } from "internal/session";
 
 import type { PartialNavDrawer } from "./partial/nav-drawer";
 
@@ -21,18 +22,29 @@ export class ViewStats extends LitElement {
   override connectedCallback(): void {
     super.connectedCallback();
 
+    Lesson.avaible().then((lessons) => {
+      this._lessons = lessons;
+    });
+
     defineComponents(IgcListComponent);
   }
 
-  @property({ attribute: false })
-  lessons: Lesson[] = [];
-  @property({ attribute: false })
-  points: { [id: string]: number } = {};
+  @state()
+  private _lessons: { [id: string]: Lesson } = {};
+  @state()
+  private _session = currentSession();
 
   @query("partial-nav-drawer", true)
   private _navDrawer!: PartialNavDrawer;
 
   protected override render(): unknown {
+    const idsWithPoints = [];
+    for (let id in this._lessons) {
+      if (this._session.getPoints(id) > 0) {
+        idsWithPoints.push(id);
+      }
+    }
+
     return html`
       <partial-navbar
         @open-menu=${() => {
@@ -44,15 +56,14 @@ export class ViewStats extends LitElement {
       <h1>Estadísticas</h1>
 
       <igc-list>
-        ${this.lessons
-          .filter((i) => (this.points[i.id] ?? 0) != 0)
-          .map(
-            (i) => html`
-              <igc-list-item
-                >${i.name}: ${this.points[i.id]} puntos.</igc-list-item
-              >
-            `,
-          )}
+        ${idsWithPoints.map(
+          (id) => html`
+            <igc-list-item
+              >${this._lessons[id].name}: ${this._session.getPoints(id)}
+              puntos.</igc-list-item
+            >
+          `,
+        )}
       </igc-list>
     `;
   }
